08d7a43d4c023013e03f4f1a2decfd0ac76bfb9b
Nicholas Ollio <ollio.nick@gmail.com>
2021-01-09 21:06:25 -0500

    converted owners to JPA entities. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass  // other things inherit from this, don't save it to db
 public class Person extends BaseEntity{
 
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
 
     public String getFirstName() {

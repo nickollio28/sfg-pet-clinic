0bebe4d5b0158e8f8662d734ce24c45267e98b71
Nicholas Ollio <ollio.nick@gmail.com>
2021-01-11 22:33:21 -0500

    tested OwnerServiceMap, implemented findByLastName. Closes #49

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/bootstrap/DataLoaderTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DataLoaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void run() {
+    }
+}
